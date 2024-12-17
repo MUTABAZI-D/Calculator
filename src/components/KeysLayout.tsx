c147d9f6190d416ef3953898f2ee72fd45121c41
MUTABAZI Dieudonne <mudonne18@gmail.com>
2024-12-17 22:20:37 +0200

    added framer motion for better experience

--- src/components/KeysLayout.tsx
+++ src/components/KeysLayout.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { motion } from "motion/react";
 
 export const KeysLayout = () => {
   const [operations, setOperations] = useState("");
@@ -50,79 +51,136 @@ export const KeysLayout = () => {
         </div>
       )}
       <div className="mb-5">
-        <button className="btn" onClick={() => handleOperations("1")}>
+        <motion.button
+          whileTap={{ scale: 1.1 }}
+          className="btn"
+          onClick={() => handleOperations("1")}
+        >
           1
-        </button>
-        <button className="btn" onClick={() => handleOperations("2")}>
+        </motion.button>
+        <motion.button
+          whileTap={{ scale: 1.1 }}
+          className="btn"
+          onClick={() => handleOperations("2")}
+        >
           2
-        </button>
-        <button className="btn" onClick={() => handleOperations("3")}>
+        </motion.button>
+        <motion.button
+          whileTap={{ scale: 1.1 }}
+          className="btn"
+          onClick={() => handleOperations("3")}
+        >
           3
-        </button>
-        <button
+        </motion.button>
+        <motion.button
+          whileTap={{ scale: 1.1 }}
           className="btn-operation"
           onClick={() => handleOperations(" + ")}
         >
           +
-        </button>
+        </motion.button>
       </div>
       <div className="mb-5">
-        <button className="btn" onClick={() => handleOperations("4")}>
+        <motion.button
+          whileTap={{ scale: 1.1 }}
+          className="btn"
+          onClick={() => handleOperations("4")}
+        >
           4
-        </button>
-        <button className="btn" onClick={() => handleOperations("5")}>
+        </motion.button>
+        <motion.button
+          whileTap={{ scale: 1.1 }}
+          className="btn"
+          onClick={() => handleOperations("5")}
+        >
           5
-        </button>
-        <button className="btn" onClick={() => handleOperations("6")}>
+        </motion.button>
+        <motion.button
+          whileTap={{ scale: 1.1 }}
+          className="btn"
+          onClick={() => handleOperations("6")}
+        >
           6
-        </button>
-        <button
+        </motion.button>
+        <motion.button
+          whileTap={{ scale: 1.1 }}
           className="btn-operation"
           onClick={() => handleOperations(" - ")}
         >
           -
-        </button>
+        </motion.button>
       </div>
       <div className="mb-5">
-        <button className="btn" onClick={() => handleOperations("7")}>
+        <motion.button
+          whileTap={{ scale: 1.1 }}
+          className="btn"
+          onClick={() => handleOperations("7")}
+        >
           7
-        </button>
-        <button className="btn" onClick={() => handleOperations("8")}>
+        </motion.button>
+        <motion.button
+          whileTap={{ scale: 1.1 }}
+          className="btn"
+          onClick={() => handleOperations("8")}
+        >
           8
-        </button>
-        <button className="btn" onClick={() => handleOperations("9")}>
+        </motion.button>
+        <motion.button
+          whileTap={{ scale: 1.1 }}
+          className="btn"
+          onClick={() => handleOperations("9")}
+        >
           9
-        </button>
-        <button
+        </motion.button>
+        <motion.button
+          whileTap={{ scale: 1.1 }}
           className="btn-operation"
           onClick={() => handleOperations(" * ")}
         >
           *
-        </button>
+        </motion.button>
       </div>
       <div className="mb-5">
-        <button className="btn" onClick={() => handleOperations("0")}>
+        <motion.button
+          whileTap={{ scale: 1.1 }}
+          className="btn"
+          onClick={() => handleOperations("0")}
+        >
           0
-        </button>
-        <button className="btn" onClick={() => handleOperations(".")}>
+        </motion.button>
+        <motion.button
+          whileTap={{ scale: 1.1 }}
+          className="btn"
+          onClick={() => handleOperations(".")}
+        >
           .
-        </button>
-        <button className="btn" onClick={handleCalculation}>
+        </motion.button>
+        <motion.button
+          whileTap={{ scale: 1.1 }}
+          className="btn"
+          onClick={handleCalculation}
+        >
           =
-        </button>
-        <button
+        </motion.button>
+        <motion.button
+          whileTap={{ scale: 1.1 }}
           className="btn-operation"
           onClick={() => handleOperations(" / ")}
         >
           /
-        </button>
+        </motion.button>
       </div>
 
       <div className="flex">
-        <button className="btn-clear" onClick={handleClear}>
+        <motion.button
+          whileTap={{ scale: 1.1 }}
+          className="btn-clear"
+          onClick={handleClear}
+        >
           Clear
-        </button>
-        <div
+        </motion.button>
+        <motion.div
+          whileTap={{ scale: 1.1 }}
           className="h-14 w-14 rounded-full bg-white flex items-center justify-center cursor-pointer"
           onClick={handleDelete}
         >
@@ -132,7 +190,7 @@ export const KeysLayout = () => {
             width={40}
             height={40}
           />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
